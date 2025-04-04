71d6a59e909dbee848b065f5ffaab4241a6907d7
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-04-04 15:53:27 -0400

    Simplify this branch - don't localize StaticValuePicker

--- frontend/src/metabase/querying/filters/components/FilterValuePicker/StaticValuePicker/StaticValuePicker.tsx
+++ frontend/src/metabase/querying/filters/components/FilterValuePicker/StaticValuePicker/StaticValuePicker.tsx
@@ -28,9 +28,6 @@ export function StaticValuePicker({
       aria-label={t`Filter value`}
       parseValue={parseValue}
       onChange={onChange}
-      shouldCreate={shouldCreate}
-      onFocus={onFocus}
-      onBlur={onBlur}
     />
   );
 }
