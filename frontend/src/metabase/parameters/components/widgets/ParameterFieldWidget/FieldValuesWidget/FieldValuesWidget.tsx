7ae6206d4743b8e7f543877a07142322e7eaa361
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-04-10 11:38:00 -0400

    Reset change to FieldValuesWidget - we'll need this later

--- frontend/src/metabase/parameters/components/widgets/ParameterFieldWidget/FieldValuesWidget/FieldValuesWidget.tsx
+++ frontend/src/metabase/parameters/components/widgets/ParameterFieldWidget/FieldValuesWidget/FieldValuesWidget.tsx
@@ -22,7 +22,6 @@ import type { LayoutRendererArgs } from "metabase/components/TokenField/TokenFie
 import ValueComponent from "metabase/components/Value";
 import CS from "metabase/css/core/index.css";
 import Fields from "metabase/entities/fields";
-import { useTranslateContent } from "metabase/i18n/components/ContentTranslationContext";
 import { parseNumber } from "metabase/lib/number";
 import { defer } from "metabase/lib/promise";
 import { connect, useDispatch } from "metabase/lib/redux";
@@ -159,7 +158,6 @@ export const FieldValuesWidgetInner = forwardRef<
   },
   ref,
 ) {
-  const tc = useTranslateContent();
   const [options, setOptions] = useState<FieldValue[]>([]);
   const [loadingState, setLoadingState] = useState<LoadingStateType>("INIT");
   const [lastValue, setLastValue] = useState<string>("");
@@ -392,7 +390,7 @@ export const FieldValuesWidgetInner = forwardRef<
         value,
         autoLoad: true,
         compact: false,
-        displayValue: tc(option?.[1]),
+        displayValue: option?.[1],
       });
     };
   }
@@ -404,7 +402,7 @@ export const FieldValuesWidgetInner = forwardRef<
         formatOptions,
         value: option[0],
         autoLoad: false,
-        displayValue: tc(option[1]),
+        displayValue: option[1],
       });
   }
 
