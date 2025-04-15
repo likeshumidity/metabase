d9ed4469aeae5a311d250d9e1f5a3cf319010148
Romeo Van Snick <romeo@romeovansnick.be>
2025-04-15 13:24:41 +0200

    Provide a default implementation of argType

--- frontend/src/metabase-lib/v1/expressions/pratt/compiler.ts
+++ frontend/src/metabase-lib/v1/expressions/pratt/compiler.ts
@@ -12,11 +12,7 @@ import {
   isIntegerLiteral,
   isStringLiteral,
 } from "../matchers";
-import type {
-  ExpressionType,
-  MBQLClauseFunctionConfig,
-  StartRule,
-} from "../types";
+import type { ExpressionType, StartRule } from "../types";
 
 import {
   ADD,
@@ -283,21 +279,9 @@ function compileArgList(
       return withNode(child, compileNode(child, ctx));
     }
 
-    const type = getArgType(defn, index, node.children, ctx.type);
+    const type = defn.argType(index, node.children, ctx.type);
     return withNode(child, compileNode(child, { ...ctx, type }));
   });
-}
-
-function getArgType(
-  defn: MBQLClauseFunctionConfig,
-  index: number,
-  args: Node[],
-  type: ExpressionType,
-): ExpressionType {
-  if (defn.argType) {
-    return defn.argType(index, args, type);
-  }
-  return defn.args[index];
 }
 
 function compileNumber(node: Node): NumberValue | Lib.ExpressionParts {
@@ -402,7 +386,7 @@ function compileUnaryOp(
   assert(isDefinedClause(operator), t`Unknown operator ${operator}`);
 
   const defn = getClauseDefinition(operator);
-  const type = getArgType(defn, 0, node.children, ctx.type);
+  const type = defn.argType(0, node.children, ctx.type);
   const arg = compileNode(node.children[0], { ...ctx, type });
 
   return withNode(node, {
@@ -427,8 +411,8 @@ function compileInfixOp(
   assert(isDefinedClause(operator), t`Unknown operator ${operator}`);
 
   const defn = getClauseDefinition(operator);
-  const leftType = getArgType(defn, 0, node.children, ctx.type);
-  const rightType = getArgType(defn, 1, node.children, ctx.type);
+  const leftType = defn.argType(0, node.children, ctx.type);
+  const rightType = defn.argType(1, node.children, ctx.type);
 
   const leftNode = compileNode(node.children[0], { ...ctx, type: leftType });
   const left =
