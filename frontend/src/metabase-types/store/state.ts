333864082ea72a459d1d4c096bc5eee1eabd6967
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-04-15 14:24:51 -0400

    Reset file

--- frontend/src/metabase-types/store/state.ts
+++ frontend/src/metabase-types/store/state.ts
@@ -2,12 +2,6 @@ import type { RouterState } from "react-router-redux";
 
 import type { User } from "metabase-types/api";
 
-// Use dynamic import to prevent circular dependencies
-type ContentTranslationsState = {
-  dictionary: Map<string, string>;
-  locale: string;
-};
-
 import type { AdminState } from "./admin";
 import type { AppState } from "./app";
 import type { AuthState } from "./auth";
@@ -43,7 +37,6 @@ export interface State {
   modal: ModalName;
   undo: UndoState;
   downloads: DownloadsState;
-  contentTranslations?: ContentTranslationsState;
 }
 
 export type Dispatch<T = any> = (action: T) => unknown | Promise<unknown>;
