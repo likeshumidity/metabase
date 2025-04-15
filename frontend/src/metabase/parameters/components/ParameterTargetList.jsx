d10e3433b168e8d2ad7d2f4c0dfd002358c7be83
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-04-15 15:00:50 -0400

    Simplify, and fix type issue

--- frontend/src/metabase/parameters/components/ParameterTargetList.jsx
+++ frontend/src/metabase/parameters/components/ParameterTargetList.jsx
@@ -1,37 +1,39 @@
 /* eslint-disable react/prop-types */
+import { Component } from "react";
 import _ from "underscore";
 
 import AccordionList from "metabase/core/components/AccordionList";
 import CS from "metabase/css/core/index.css";
 import { Icon } from "metabase/ui";
 
-export const ParameterTargetList = ({
-  mappingOptions,
-  selectedMappingOption,
-  maxHeight,
-  onChange,
-}) => {
-  const mappingOptionSections = _.groupBy(mappingOptions, "sectionName");
+export default class ParameterTargetList extends Component {
+  props;
 
-  const hasForeignOption = _.any(mappingOptions, (o) => !!o.isForeign);
+  render() {
+    const { mappingOptions, selectedMappingOption } = this.props;
 
-  const sections = _.map(mappingOptionSections, (options) => ({
-    name: options[0].sectionName,
-    items: options,
-  }));
+    const mappingOptionSections = _.groupBy(mappingOptions, "sectionName");
 
-  return (
-    <AccordionList
-      className={CS.textBrand}
-      maxHeight={maxHeight || 600}
-      sections={sections}
-      onChange={(item) => onChange(item.target)}
-      itemIsSelected={(item) => item === selectedMappingOption}
-      renderItemIcon={(item) => (
-        <Icon name={item.icon || "unknown"} size={18} />
-      )}
-      alwaysExpanded={true}
-      hideSingleSectionTitle={!hasForeignOption}
-    />
-  );
-};
+    const hasForeignOption = _.any(mappingOptions, (o) => !!o.isForeign);
+
+    const sections = _.map(mappingOptionSections, (options) => ({
+      name: options[0].sectionName,
+      items: options,
+    }));
+
+    return (
+      <AccordionList
+        className={CS.textBrand}
+        maxHeight={this.props.maxHeight || 600}
+        sections={sections}
+        onChange={(item) => this.props.onChange(item.target)}
+        itemIsSelected={(item) => item === selectedMappingOption}
+        renderItemIcon={(item) => (
+          <Icon name={item.icon || "unknown"} size={18} />
+        )}
+        alwaysExpanded={true}
+        hideSingleSectionTitle={!hasForeignOption}
+      />
+    );
+  }
+}
