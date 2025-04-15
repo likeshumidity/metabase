bc96329a056bcab377c362e490bdeaf0f5ce0adb
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-04-15 16:28:57 -0400

    Fix

--- enterprise/frontend/src/metabase-enterprise/content_translation/index.ts
+++ enterprise/frontend/src/metabase-enterprise/content_translation/index.ts
@@ -1,13 +1,9 @@
 import { PLUGIN_CONTENT_TRANSLATION } from "metabase/plugins";
-import { useTranslateContent } from "./hooks";
-
 import { hasPremiumFeature } from "metabase-enterprise/settings";
 
-if (
-  // TODO Remove this once the Cloud team adds the token feature
-  true ||
-  hasPremiumFeature("content_translation")
-) {
+import { useTranslateContent } from "./hooks";
+
+if (hasPremiumFeature("content_translation")) {
   Object.assign(PLUGIN_CONTENT_TRANSLATION, {
     isContentTranslationEnabled: true,
     useTranslateContent,
