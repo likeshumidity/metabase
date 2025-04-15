bbff6cd40184932b21c7e16d66c72e66474fa577
Romeo Van Snick <romeo@romeovansnick.be>
2025-04-15 13:18:29 +0200

    Use helper to calculate argtype

--- frontend/src/metabase-lib/v1/expressions/pratt/compiler.ts
+++ frontend/src/metabase-lib/v1/expressions/pratt/compiler.ts
@@ -12,7 +12,11 @@ import {
   isIntegerLiteral,
   isStringLiteral,
 } from "../matchers";
-import type { ExpressionType, StartRule } from "../types";
+import type {
+  ExpressionType,
+  MBQLClauseFunctionConfig,
+  StartRule,
+} from "../types";
 
 import {
   ADD,
@@ -279,10 +283,21 @@ function compileArgList(
       return withNode(child, compileNode(child, ctx));
     }
 
-    const type =
-      defn.argType?.(index, node.children, ctx.type) ?? defn.args[index];
+    const type = getArgType(defn, index, node.children, ctx.type);
     return withNode(child, compileNode(child, { ...ctx, type }));
   });
+}
+
+function getArgType(
+  defn: MBQLClauseFunctionConfig,
+  index: number,
+  args: Node[],
+  type: ExpressionType,
+): ExpressionType {
+  if (defn.argType) {
+    return defn.argType(index, args, type);
+  }
+  return defn.args[index];
 }
 
 function compileNumber(node: Node): NumberValue | Lib.ExpressionParts {
@@ -387,7 +402,7 @@ function compileUnaryOp(
   assert(isDefinedClause(operator), t`Unknown operator ${operator}`);
 
   const defn = getClauseDefinition(operator);
-  const type = defn?.argType?.(0, node.children, ctx.type) ?? defn?.args[0];
+  const type = getArgType(defn, 0, node.children, ctx.type);
   const arg = compileNode(node.children[0], { ...ctx, type });
 
   return withNode(node, {
@@ -412,9 +427,8 @@ function compileInfixOp(
   assert(isDefinedClause(operator), t`Unknown operator ${operator}`);
 
   const defn = getClauseDefinition(operator);
-  const leftType = defn?.argType?.(0, node.children, ctx.type) ?? defn?.args[0];
-  const rightType =
-    defn?.argType?.(0, node.children, ctx.type) ?? defn?.args[1];
+  const leftType = getArgType(defn, 0, node.children, ctx.type);
+  const rightType = getArgType(defn, 1, node.children, ctx.type);
 
   const leftNode = compileNode(node.children[0], { ...ctx, type: leftType });
   const left =
